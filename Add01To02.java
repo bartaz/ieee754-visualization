d5123cd1b91dc4263240e3099d46df8a26145ce0
Bartek Szopka <bartek.szopka+github@gmail.com>
2013-09-16 10:19:37 +0200

    added other files used during presentation

---
+++ Add01To02.java
@@ -0,0 +1,11 @@
+class Add01To02 {
+    public static void main(String[] args) {
+        System.out.println("\n> 0.1 + 0.2 == 0.3");
+        System.out.println(     0.1 + 0.2 == 0.3 );
+
+        System.out.println("\n> 0.1 + 0.2");
+        System.out.println(     0.1 + 0.2 );
+
+        System.out.println();
+    }
+}
